d23328bef797ef69922d09af0002529907eb2bf2
tapsikk <tapsgame@gmail.com>
2025-06-12 00:18:31 +0300

    123

--- src/Countdown.jsx
+++ src/Countdown.jsx
@@ -44,7 +44,7 @@ const Countdown = () => {
       <div className="creator">
         <p>Создано: </p>
         <img
-         src='/public/image/discord.png'
+         src='/image/discord-logo.png'cd
           alt="BallCloseup_16_9.png"
           className="discord-logo"
         />
